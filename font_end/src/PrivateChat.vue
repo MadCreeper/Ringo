90424ffee0583f4e437be80e57ef2fd1bb437c43
MadCreeper <sunjichen21@126.com>
2022-10-22 13:32:45 +0800

    Add chat screen (/chat, /chat2)

---
+++ font_end/src/PrivateChat.vue
@@ -0,0 +1,141 @@
+<template>
+    <el-container>
+        <el-page-header @back="goBack">
+            <template #content>
+                <span class="text-large font-600 mr-3"> Chat with 张三 </span>
+            </template>
+        </el-page-header>
+        <el-main>
+            <el-row class="row-bg" justify="end" gutter="50">
+                <el-col :span="6">
+                    <div class="grid-content ep-bg-purple">
+                        <el-button type="success" round>大家好啊，我是说的道理</el-button>
+                    </div>
+                </el-col>
+                <el-col :span="4" :offset="1">
+                    <el-avatar class="avater" :size="large" :src="circleUrl" @click="redirect"></el-avatar>
+                </el-col>
+            </el-row>
+
+            <el-row class="row-bg" justify="end" gutter="50">
+                <el-col :span="6">
+                    <div class="grid-content ep-bg-purple">
+                        <el-button type="success" round>今天来点大家想看的东西啊</el-button>
+                    </div>
+                </el-col>
+                <el-col :span="4" :offset="1">
+                    <el-avatar class="avater" :size="large" :src="circleUrl" @click="redirect"></el-avatar>
+                </el-col>
+            </el-row>
+
+            <el-row class="row-bg">
+                <el-col :span="1" :offset="1">
+                    <el-avatar class="avater" :size="large" :src="circleUrl" @click="redirect"></el-avatar>
+                </el-col>
+                <el-col :span="6">
+                    <div class="grid-content ep-bg-purple">
+                        <el-button type="primary" round>义！乌！</el-button>
+                    </div>
+                </el-col>
+            </el-row>
+
+            <el-row class="row-bg" justify="end" gutter="50">
+                <el-col :span="6">
+                    <div class="grid-content ep-bg-purple">
+                        <el-button type="success" round>哥们在这给你说唱🐴</el-button>
+                    </div>
+                </el-col>
+                <el-col :span="4" :offset="1">
+                    <el-avatar class="avater" :size="large" :src="circleUrl" @click="redirect"></el-avatar>
+                </el-col>
+            </el-row>
+
+            <el-row class="row-bg">
+                <el-col :span="1" :offset="1">
+                    <el-avatar class="avater" :size="large" :src="circleUrl" @click="redirect"></el-avatar>
+                </el-col>
+                <el-col :span="6">
+                    <div class="grid-content ep-bg-purple">
+                        <el-button type="primary" round>L T C</el-button>
+                    </div>
+                </el-col>
+            </el-row>
+
+            <el-row class="row-bg">
+                <el-col :span="1" :offset="1">
+                    <el-avatar class="avater" :size="large" :src="circleUrl" @click="redirect"></el-avatar>
+                </el-col>
+                <el-col :span="6">
+                    <div class="grid-content ep-bg-purple">
+                        <el-button type="primary" round>理塘丁真!</el-button>
+                    </div>
+                </el-col>
+            </el-row>
+
+            <el-row class="row-bg">
+                <el-col :span="1" :offset="1">
+                    <el-avatar class="avater" :size="large" :src="circleUrl" @click="redirect"></el-avatar>
+                </el-col>
+                <el-col :span="6">
+                    <div class="grid-content ep-bg-purple">
+                        <el-button type="primary" round>
+                            哥们名叫丁真 Smokin' 'round, e-cigarettes new 我的小马名字叫珍珠
+                        </el-button>
+                    </div>
+                </el-col>
+            </el-row>
+
+            <el-row>
+                <bubble-chat :position="chatPosition" :messages="messagesList" :text-field="'message'"
+                    :sender-name-field="'username'" :avatar-link-field="'iconUrl'">
+                </bubble-chat>
+            </el-row>
+
+        </el-main>
+        <!-- Footer -->
+        <el-footer>
+            <el-row class="row-bg" justify="end">
+                <el-col :span="20" :offset="1">
+                    <el-input v-model="input" placeholder="Please input" />
+                </el-col>
+                <el-col :span="3">
+                    <div class="grid-content ep-bg-purple">
+                        <el-button type="primary" round>发送</el-button>
+                    </div>
+                </el-col>
+            </el-row>
+        </el-footer>
+    </el-container>
+</template>
+
+<script setup>
+
+import { reactive, toRefs } from 'vue'
+
+const state = reactive({
+    circleUrl:
+        'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png',
+})
+const { circleUrl } = toRefs(state);
+
+
+
+</script>
+
+<style>
+.el-container {
+    min-height: 100vh;
+}
+
+.el-row {
+    margin-bottom: 20px;
+}
+
+.el-row:last-child {
+    margin-bottom: 0;
+}
+
+.el-col {
+    border-radius: 50px;
+}
+</style>
--- font_end/src/main.js
+++ font_end/src/main.js
@@ -1,22 +1,31 @@
 // npx 下载的module的地址 ~/.npm/_npx\
 // npm 的全局下载路径为 /usr/local/lib/node_modules
-import { createApp } from 'vue'
+import { createApp} from 'vue'
 import {createRouter,createWebHashHistory} from 'vue-router'
 import home from './HomePage.vue'
 import Info from './PersonalInfo.vue'
+import chat from './PrivateChat.vue'
+import chat2 from './Chat2.vue'
 const routes = [
     { path: '/', component: home },
     { path: '/info', component: Info },
+    { path: '/chat', component: chat},
+    { path: '/chat2', component: chat2}
 ]
 const router = createRouter({
     // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
     history: createWebHashHistory(),
     routes, // `routes: routes` 的缩写
 })
+import Chat from 'vue3-beautiful-chat'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
+
 const app = createApp(App)
 app.use(router)
 app.use(ElementPlus)
+app.use(Chat)
 app.mount('#app')
+
+
