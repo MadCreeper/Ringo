15926c42e4827203358a783105682c517b0cc7ca
J <ogas_dandelion@sjtu.edu.cn>
2022-12-09 17:09:36 +0800

    后端登录系统不过

--- RingoBackend/login/jwt_utils.py
+++ RingoBackend/login/jwt_utils.py
@@ -1,7 +1,5 @@
 def jwt_response_payload_handler(token, user, request):
     return {
         'token':token,
-        'uid':user.id,
-        'username':user.username,
-        'email':user.email
+        ** request.data
     }
