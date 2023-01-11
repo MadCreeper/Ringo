b777e1b2579a8c281dcc7c5a99c04d79c3d4103b
J <ogas_dandelion@sjtu.edu.cn>
2023-01-11 21:55:33 +0800

    修复个人档案外键导致的bug

--- RingoBackend/login/tests.py
+++ RingoBackend/login/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from login.constants import *
+from login.models import User
 
 # Create your tests here.
+class SmokeTestCase(TestCase):
+    def test_smoke(self):
+        self.assertEqual(1+1, 2)
+
+class LoginTest(TestCase):
+    def setUp(self) -> None:
+        user = User.objects.create_user()
+        return super().setUp()
