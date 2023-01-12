d6e506ebb0cb92d4e805e0d84b5a5eca3affa239
132ash <alexanderao132@gmail.com>
2023-01-12 12:04:37 +0800

    test backend

--- RingoBackend/login/helpers/mailSend.py
+++ RingoBackend/login/helpers/mailSend.py
@@ -22,17 +22,13 @@ def sendMail(receiver, validation, username):
     subjectContent = "SJTU Lingo —— 交大林檎验证码"
     mimeMulti["Subject"] = Header(subjectContent, 'utf-8')
     bodyContent = f"{username}先生/小姐，您好。\n这里是交大林檎线上互助平台。\n\n请对您的账户{receiver}使用如下验证码\n验证码：{validation}\n\n若验证码的申请非您本人操作，可忽略这篇邮件，其他人可能错误地键入了您的电子邮箱地址。"
-    with open('login\helpers\Lingo_wide.png', mode = 'rb') as fp:
-        picture = MIMEImage(fp.read())
-        picture['Content-Type'] = 'application/octet-stream'
-        picture['Content-Disposition'] = 'attachment;filename="Lingo_wide.png"'
 
     messageText = MIMEText(bodyContent, "plain", "utf-8")
     mimeMulti.attach(messageText)
 
     stp = smtplib.SMTP()
     stp.connect(mail_host, 25)
-    stp.set_debuglevel(1)
+    stp.set_debuglevel(0)
     stp.login(mail_sender, mail_token)
     stp.sendmail(mail_sender, receiver, mimeMulti.as_string())
     stp.quit()
